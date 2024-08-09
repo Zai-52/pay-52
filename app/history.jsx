75aca1f758266193dbcefe579927b16c7bbb1b89
Zaw Ye Htet (DAT) <zawyehtet7928@gmail.com>
2024-08-09 10:31:46 +0630

    Initial commit

---
+++ app/history.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import Transations from "./components/Transactions";
+
+export default function History() {
+    return (
+        <View style={{ padding: 20 }}>
+            <Transations />
+        </View>
+    );
+}
